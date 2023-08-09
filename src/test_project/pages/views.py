af8603456a0f38a86cba4964aa8b037c8aa9107c
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-09 14:55:26 +0400

    urls rendering

---
+++ src/test_project/pages/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request)
+    print(request.user)
+    # return HttpResponse("<h1>hello World</h1>")
+    return render(request, "home.html", {})
+
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request,"about.html",{})
+
+
+def social_view(request, *args, **kwargs):
+    return render(request, "social.html",{})
+
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>contact page</h1>")
