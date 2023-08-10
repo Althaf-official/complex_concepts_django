b9ec21f93c299bb67a6c9745148a441946fd8bea
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-10 09:17:58 +0400

    Rendering context in template

--- src/test_project/pages/views.py
+++ src/test_project/pages/views.py
@@ -17,7 +17,8 @@ def contact_view(request, *args, **kwargs):
 def about_view(request, *args, **kwargs):
     my_context = {
         "my_text":"this is about me",
-        "my_number": 1234
+        "my_number": 1234,
+        "my_list":[123,456,789]
 
     }
     return render(request,"about.html",my_context)
